38dd9b39ad29623f138d1f988924230b781b2bb3
Isaac Delfa Medina <150245173+IsaacDeMe@users.noreply.github.com>
2025-06-07 06:44:07 +0000

    Actualizando archivos malos

--- src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://kakrjsytqtijofhudqpu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtha3Jqc3l0cXRpam9maHVkcXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NzE1MTcsImV4cCI6MjA2NDU0NzUxN30.bQNMOZhM9RN0m_oW0pGgXWGPTAEDih0Tao5YE5F7vbI';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
